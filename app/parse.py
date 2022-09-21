8bda7195c405a8977f2f1bd734da28219a06c7da
Anton Lasko <anton.lasko1993@gmail.com>
2022-09-21 13:21:48 +0300

    'Solution'

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,20 @@
+import csv
+
 from dataclasses import dataclass
 from urllib.parse import urljoin
 
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+s = Service("C:/Users/Anton/chrome_driver_for_selenium/chromedriver.exe")
 BASE_URL = "https://webscraper.io/"
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
+COMPUTERS_URL = urljoin(HOME_URL, "computers/")
+LAPTOPS_URL = urljoin(COMPUTERS_URL, "laptops")
+TABLETS_URL = urljoin(COMPUTERS_URL, "tablets")
+PHONES_URL = urljoin(HOME_URL, "phones/")
+TOUCH_URL = urljoin(PHONES_URL, "touch")
 
 
 @dataclass
@@ -15,8 +26,54 @@ class Product:
     num_of_reviews: int
 
 
+def create_file(data: list, file_name: str):
+
+    with open(file_name, "w", encoding='utf-8', newline="") as file:
+        first_row = ["title", "description", "price", "rating", "num_of_reviews"]
+
+        writer = csv.writer(file)
+        writer.writerow(first_row)
+        writer.writerows(data)
+
+
+def parce_single_page(link: str, file_name: str):
+    driver = webdriver.Chrome(service=s)
+    driver.get(link)
+    cookies = driver.find_element(By.CLASS_NAME, "acceptCookies")
+    if cookies.is_displayed():
+        cookies.click()
+    button_more = driver.find_element(By.CSS_SELECTOR, "ecomerce-items-scroll-more")
+    products = driver.find_elements(By.CSS_SELECTOR, "div.col-sm-4")
+    products_info = []
+    if button_more.is_displayed():
+        button_more.click()
+
+    for product in products:
+        title = product.find_element(By.CLASS_NAME, "title").get_attribute("title")
+        description = product.find_element(By.CSS_SELECTOR, ".description").text
+        price = float(product.find_element(By.CLASS_NAME, "price").text.replace("$", ""))
+        rating = int(len(product.find_elements(By.CLASS_NAME, "glyphicon-star")))
+        num_of_reviews = int(product.find_element(By.CSS_SELECTOR, ".ratings .pull-right").text.split()[0])
+
+        product_data = [title, description, price, rating, num_of_reviews]
+
+        products_info.append(product_data)
+
+    driver.close()
+    create_file(products_info, file_name)
+
+
 def get_all_products():
-    pass
+    links = [
+        (HOME_URL, "home.csv"),
+        (COMPUTERS_URL, "computers.csv"),
+        (LAPTOPS_URL, "laptops.csv"),
+        (TABLETS_URL, "tablets.csv"),
+        (PHONES_URL, "phones.csv"),
+        (TOUCH_URL, "touch.csv"),
+    ]
+    for link in links:
+        parce_single_page(link[0], link[1])
 
 
 if __name__ == "__main__":
