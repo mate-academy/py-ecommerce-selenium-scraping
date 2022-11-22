8bd5e9216ea3fccd0b434d35c1f39a9e5bc32011
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2022-11-22 16:19:10 +0200

    'Solution'

--- app/parse.py
+++ app/parse.py
@@ -8,15 +8,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 
-BASE_URL = "https://webscraper.io/"
-HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more")
-COMPUTERS_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/computers")
-LAPTOPS_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/computers/laptops")
-TABLES_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/computers/tablets")
-PHONES_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/phones")
-TOUCH_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/phones/touch")
+HOME_URL = "https://webscraper.io/test-sites/e-commerce/more/"
+COMPUTERS_URL = urljoin(HOME_URL, "computers")
+LAPTOPS_URL = urljoin(HOME_URL, "computers/laptops")
+TABLES_URL = urljoin(HOME_URL, "computers/tablets")
+PHONES_URL = urljoin(HOME_URL, "phones")
+TOUCH_URL = urljoin(HOME_URL, "phones/touch")
 
-ALL_URL_FOR_THIS_TACK = [HOME_URL, COMPUTERS_URL, LAPTOPS_URL, TABLES_URL, PHONES_URL, TOUCH_URL]
+ALL_URLS = [HOME_URL, COMPUTERS_URL, LAPTOPS_URL, TABLES_URL, PHONES_URL, TOUCH_URL]
+
+FILE_NAMES = [
+    "home.csv" if url.split("/")[-1] == "" else f"{url.split('/')[-1]}.csv"
+    for url in ALL_URLS
+]
 
 
 @dataclass
@@ -48,47 +52,55 @@ def parse_single_product(product_soup: BeautifulSoup) -> Product:
         title=product_soup.select_one(".title")["title"],
         description=product_soup.select_one(".description").text,
         price=float(product_soup.select_one(".price").text.replace("$", "")),
-        rating=int(product_soup.select_one("p[data-rating]")["data-rating"]) if product_soup.select_one("p[data-rating]") else len(product_soup.select(".glyphicon-star")),
-        num_of_reviews=int(product_soup.select_one(".ratings > p.pull-right").text.split()[0])
+        rating=int(product_soup.select_one("p[data-rating]")["data-rating"])
+        if product_soup.select_one("p[data-rating]")
+        else len(product_soup.select(".glyphicon-star")),
+        num_of_reviews=int(
+            product_soup.select_one(".ratings > p.pull-right").text.split()[0]
+        ),
     )
 
 
-def get_all_products() -> None:
+def parse(url: str) -> [Product]:
 
-    for url in ALL_URL_FOR_THIS_TACK:
-        page = requests.get(url).content
-        soup = BeautifulSoup(page, "html.parser")
+    page = requests.get(url).content
+    soup = BeautifulSoup(page, "html.parser")
 
-        if soup.select_one(".ecomerce-items-scroll-more"):
-            driver = get_driver()
-            driver.get(url)
+    if soup.select_one(".ecomerce-items-scroll-more"):
+        driver = get_driver()
+        driver.get(url)
 
-            while soup.select_one(".ecomerce-items-scroll-more"):
+        while soup.select_one(".ecomerce-items-scroll-more"):
 
-                button = driver.find_element(By.CLASS_NAME, "ecomerce-items-scroll-more")
+            button = driver.find_element(By.CLASS_NAME, "ecomerce-items-scroll-more")
 
-                driver.execute_script("arguments[0].click();", button)
+            driver.execute_script("arguments[0].click();", button)
 
-                if button.get_attribute("style"):
-                    break
+            if button.get_attribute("style"):
+                break
 
-            page_source = driver.page_source
+        page_source = driver.page_source
 
-            soup = BeautifulSoup(page_source, 'html.parser')
+        soup = BeautifulSoup(page_source, "html.parser")
 
-        products = soup.select(".thumbnail")
+    products = soup.select(".thumbnail")
 
-        all_products = [parse_single_product(product_soup) for product_soup in products]
+    all_products = [parse_single_product(product_soup) for product_soup in products]
 
-        file_name = "home.csv" if url.split('/')[-1] == "more" else f"{url.split('/')[-1]}.csv"
+    return all_products
 
-        with open(file_name, "w", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow(PRODUCT_FIELDS)
-            writer.writerows([astuple(product) for product in all_products])
+
+def get_all_products(file_name: str, all_products: [Product]) -> None:
+    with open(file_name, "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(PRODUCT_FIELDS)
+        writer.writerows([astuple(product) for product in all_products])
 
 
 if __name__ == "__main__":
     with webdriver.Chrome() as new_driver:
         set_driver(new_driver)
-        get_all_products()
+
+        for count, url in enumerate(ALL_URLS):
+            all_products = parse(url)
+            get_all_products(file_name=FILE_NAMES[count], all_products=all_products)
