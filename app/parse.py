949ee6f7ff8dc4f92459a8548fa922621472b59b
Dmitriy Kostiuk <kostiuk.dmitriy@gmail.com>
2022-11-30 09:21:29 +0200

    'Solution'

--- app/parse.py
+++ app/parse.py
@@ -22,11 +22,15 @@ class Product:
     num_of_reviews: int
 
     def __iter__(self) -> iter:
-        return iter([self.title,
-                     self.description,
-                     self.price,
-                     self.rating,
-                     self.num_of_reviews])
+        return iter(
+            [
+                self.title,
+                self.description,
+                self.price,
+                self.rating,
+                self.num_of_reviews
+            ]
+        )
 
 
 class SeleniumDriver:
@@ -46,18 +50,21 @@ def parse_single_product(product_card: WebElement) -> Product:
         By.CLASS_NAME,
         "title"
     ).get_attribute("title")
+
     descr = product_card.find_element(By.CLASS_NAME, "description").text
     price = float(product_card.find_element(By.CLASS_NAME, "price").text[1:])
     rating_views = product_card.find_element(By.CLASS_NAME, "ratings")
     views = rating_views.find_elements(By.CSS_SELECTOR, "p")
     rating = len(views[-1].find_elements(By.CSS_SELECTOR, "span"))
     num_views = int(views[0].text.split()[0])
-    return Product(title=name,
-                   description=descr,
-                   price=price,
-                   rating=rating,
-                   num_of_reviews=num_views
-                   )
+
+    return Product(
+        title=name,
+        description=descr,
+        price=price,
+        rating=rating,
+        num_of_reviews=num_views
+    )
 
 
 def parsing_page(url: str, file_name: str) -> None:
@@ -69,19 +76,23 @@ def parsing_page(url: str, file_name: str) -> None:
     )
     if len(buttons) > 0:
         button = buttons[0]
+
         while button.is_displayed():
             ActionChains(driver).click(button).perform()
             button = driver.find_element(
                 By.CLASS_NAME,
                 "ecomerce-items-scroll-more"
             )
+
     product_cards = driver.find_elements(
         By.CLASS_NAME,
         "thumbnail"
     )
     products = []
+
     for product_card in product_cards:
         products.append(parse_single_product(product_card))
+
     save_to_file(file_name, products)
 
 
@@ -95,19 +106,22 @@ def get_all_products() -> None:
         "touch": "phones/touch",
     }
     base_url = "https://webscraper.io/test-sites/e-commerce/more"
+
     for page in pages.keys():
         parsing_page(f"{base_url}/{pages[page]}", f"{page}.csv")
 
 
 def save_to_file(file_name: str, products_list: list[Product]) -> None:
     mode = "w" if exists(file_name) else "x"
+
     with open(file_name, mode, 100, "utf-8", newline="") as file_to_save:
-        fieldnames = ["title",
-                      "description",
-                      "price",
-                      "rating",
-                      "num_of_reviews"
-                      ]
+        fieldnames = [
+            "title",
+            "description",
+            "price",
+            "rating",
+            "num_of_reviews"
+        ]
         writer = csv.writer(file_to_save)
         writer.writerow(fieldnames)
         writer.writerows(products_list)
