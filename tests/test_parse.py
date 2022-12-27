e537d78699c30e080f7761fa1db1e767471dd8b9
Soroka Igor <copokaigor@gmail.com>
2022-12-27 11:58:53 +0200

    Solution

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -2,14 +2,16 @@ import csv
 import os.path
 from pathlib import Path
 import pytest
-from app import main
+from app.parse import Product
+from app.main import get_all_products
+
 
 TEST_DIR = Path(__file__).resolve().parent
 
 
 @pytest.fixture(scope="session", autouse=True)
 def run_scraper():
-    main.get_all_products()
+    get_all_products()
 
 
 @pytest.mark.parametrize("page", ["home", "computers", "phones"])
@@ -28,7 +30,7 @@ def test_static_products_are_correct(page):
         for correct_row in correct_reader:
             result_row = next(result_reader)
 
-            correct_product = main.Product(*correct_row)
-            result_product = main.Product(*result_row)
+            correct_product = Product(*correct_row)
+            result_product = Product(*result_row)
 
             assert correct_product == result_product
