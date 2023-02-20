ba16659d2289caa7d940ce8acab10d5605f25698
vadim-kosnovskiy <kvmltkv@gmail.com>
2023-02-20 21:01:14 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,30 @@
-from dataclasses import dataclass
+import time
+import csv
+from dataclasses import dataclass, astuple, fields
 from urllib.parse import urljoin
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 BASE_URL = "https://webscraper.io/"
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
+COMPUTERS_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/computers")
+LAPTOPS_URL = urljoin(
+    BASE_URL, "test-sites/e-commerce/more/computers/laptops")
+TABLETS_URL = urljoin(
+    BASE_URL, "test-sites/e-commerce/more/computers/tablets")
+PHONES_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/phones")
+TOUCH_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/phones/touch")
+
+URLS = {
+    HOME_URL: "home.csv",
+    COMPUTERS_URL: "computers.csv",
+    LAPTOPS_URL: "laptops.csv",
+    TABLETS_URL: "tablets.csv",
+    PHONES_URL: "phones.csv",
+    TOUCH_URL: "touch.csv",
+}
 
 
 @dataclass
@@ -15,9 +36,80 @@ class Product:
     num_of_reviews: int
 
 
+PRODUCT_FIELDS = [field.name for field in fields(Product)]
+
+_driver: WebDriver | None = None
+
+
+def get_driver() -> WebDriver:
+    return _driver
+
+
+def set_driver(new_driver: WebDriver) -> None:
+    global _driver
+    _driver = new_driver
+
+
+def parse_page(url: str) -> list:
+    driver = get_driver()
+    driver.get(url)
+
+    time.sleep(1)
+
+    if len(driver.find_elements(By.CLASS_NAME, "acceptCookies")):
+        cookies = driver.find_element(By.CLASS_NAME, "acceptCookies")
+        cookies.click()
+
+    if len(driver.find_elements(By.CLASS_NAME, "ecomerce-items-scroll-more")):
+        more_button = driver.find_element(
+            By.CLASS_NAME, "ecomerce-items-scroll-more"
+        )
+        while more_button.is_displayed():
+            more_button.click()
+            time.sleep(0.1)
+
+    products = driver.find_elements(By.CLASS_NAME, "thumbnail")
+    return products
+
+
+def get_single_product(product: webdriver) -> Product:
+    return Product(
+        title=product.find_element(
+            By.CLASS_NAME, "title").get_attribute("title"),
+        description=product.find_element(By.CLASS_NAME, "description").text,
+        price=float(product.find_element(
+            By.CLASS_NAME, "price").text.replace("$", "")),
+        rating=len(product.find_elements(By.CLASS_NAME, "glyphicon-star")),
+        num_of_reviews=int(product.find_element(
+            By.CLASS_NAME, "ratings"
+        ).find_element(
+            By.TAG_NAME, "p"
+        ).text.split(" ")[0]),
+    )
+
+
 def get_all_products() -> None:
-    pass
+    with webdriver.Chrome() as new_driver:
+        new_driver.set_window_position(-20, 0)
+        set_driver(new_driver)
+        for url, file_name in URLS.items():
+            products = parse_page(url)
+            result = []
+            for product in products:
+                result.append(get_single_product(product))
+            write_products_to_csv(file_name, result)
+
+
+def write_products_to_csv(file_name: str, products: [Product]) -> None:
+    with open(file_name, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(PRODUCT_FIELDS)
+        writer.writerows([astuple(product) for product in products])
 
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     get_all_products()
+    end = time.perf_counter()
+
+    print("Elapsed:", end - start)
