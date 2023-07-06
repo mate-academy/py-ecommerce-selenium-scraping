7dace9464b95a789f4904c7b147e02d4f04b8a38
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-07-06 21:32:08 +0300

    Using OOP

--- app/parse.py
+++ app/parse.py
@@ -1,8 +1,9 @@
+from __future__ import annotations
+
 import time
 from dataclasses import dataclass, fields, astuple
 from urllib.parse import urljoin
 import csv
-
 from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -16,7 +17,7 @@ URLS = {
     "laptops": urljoin(BASE_URL, "computers/laptops"),
     "tablets": urljoin(BASE_URL, "computers/tablets"),
     "phones": urljoin(BASE_URL, "phones/"),
-    "touch": urljoin(BASE_URL, "phones/touch")
+    "touch": urljoin(BASE_URL, "phones/touch"),
 }
 
 
@@ -28,46 +29,48 @@ class Product:
     rating: int
     num_of_reviews: int
 
+    def parse_single_product(self, product: Tag) -> Product:
+        return Product(
+            title=product.select_one(".title")["title"],
+            description=product.select_one(".description").text.replace(
+                "\xa0", " "
+            ),
+            price=float(product.select_one("h4.price").text.replace("$", "")),
+            rating=len(product.select(".glyphicon-star")),
+            num_of_reviews=int(
+                product.select_one("p.pull-right").text.split()[0]
+            ),
+        )
+
+    def get_page_products(
+        self, page_url: str, driver: WebDriver
+    ) -> list[Product]:
+        driver.get(page_url)
+        cookies = driver.find_elements(By.CLASS_NAME, "acceptCookies")
+
+        if cookies:
+            cookies[0].click()
+
+        button_more = driver.find_elements(
+            By.CLASS_NAME, "ecomerce-items-scroll-more"
+        )
+
+        if button_more:
+            while button_more[0].is_displayed():
+                button_more[0].click()
+                time.sleep(0.1)
+
+        page = driver.page_source
+        page_soup = BeautifulSoup(page, "html.parser")
+
+        products = page_soup.select(".thumbnail")
+
+        return [
+            self.parse_single_product(self, product) for product in products
+        ]
+
 
 PRODUCT_FIELDS = [field.name for field in fields(Product)]
-
-
-def parse_single_product(product: Tag) -> Product:
-    return Product(
-        title=product.select_one(".title")["title"],
-        description=product.select_one(
-            ".description"
-        ).text.replace("\xa0", " "),
-        price=float(product.select_one("h4.price").text.replace("$", "")),
-        rating=len(product.select(".glyphicon-star")),
-        num_of_reviews=int(
-            product.select_one("p.pull-right").text.split()[0]
-        ),
-    )
-
-
-def get_page_products(page_url: str, driver: WebDriver) -> list[Product]:
-    driver.get(page_url)
-    cookies = driver.find_elements(By.CLASS_NAME, "acceptCookies")
-
-    if cookies:
-        cookies[0].click()
-
-    button_more = driver.find_elements(
-        By.CLASS_NAME, "ecomerce-items-scroll-more"
-    )
-
-    if button_more:
-        while button_more[0].is_displayed():
-            button_more[0].click()
-            time.sleep(0.1)
-
-    page = driver.page_source
-    page_soup = BeautifulSoup(page, "html.parser")
-
-    products = page_soup.select(".thumbnail")
-
-    return [parse_single_product(product) for product in products]
 
 
 def write_products_to_csv(products: [Product], output_csv_path: str) -> None:
@@ -81,8 +84,8 @@ def get_all_products() -> None:
     with webdriver.Chrome() as driver:
         for name, url in URLS.items():
             write_products_to_csv(
-                products=get_page_products(url, driver),
-                output_csv_path=f"{name}.csv"
+                products=Product.get_page_products(Product, url, driver),
+                output_csv_path=f"{name}.csv",
             )
 
 
