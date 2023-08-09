040817555582e643d4316fb70f2334f2f5e38d6b
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-09 14:49:55 +0300

    Add get_single_product method to Product class in parse.py

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from typing import Self
 from urllib.parse import urljoin
+
+from bs4 import Tag
 
 
 BASE_URL = "https://webscraper.io/test-sites/e-commerce/more/"
@@ -20,6 +23,19 @@ class Product:
     price: float
     rating: int
     num_of_reviews: int
+
+    @classmethod
+    def get_single_product(cls, product: Tag) -> Self:
+        single_product_data = dict(
+            title=product.select_one(".title")["title"],
+            description=product.select_one(".description").text,
+            price=float(product.select_one(".price").text.replace("$", "")),
+            rating=len(product.select(".glyphicon-star")),
+            num_of_reviews=int(
+                product.select_one("p.pull-right").text.split()[0]
+            ),
+        )
+        return cls(**single_product_data)
 
 
 def get_all_products() -> None:
