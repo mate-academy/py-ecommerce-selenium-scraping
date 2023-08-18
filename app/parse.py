654b96f3c4ef4934c802f4aa01498eef11c63a97
Vyacheslav Kungin <acheros4@gmail.com>
2023-08-18 19:26:48 +0300

    refactored

--- app/parse.py
+++ app/parse.py
@@ -1,3 +1,4 @@
+import time
 from dataclasses import dataclass, fields, astuple
 from urllib.parse import urljoin
 import csv
@@ -5,7 +6,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
 
 
 BASE_URL = "https://webscraper.io/"
@@ -47,21 +47,24 @@ class Product:
     price: float
     rating: int
     num_of_reviews: int
-    # additional_info: dict :TODO test does not cover it
+    additional_info: dict  # :TODO test does not cover it
 
 
 PRODUCT_FIELDS = [field.name for field in fields(Product)]
 
-_driver: WebDriver | None = None
 
+class DriverManager:
+    _driver: webdriver.Chrome | None = None
 
-def get_driver() -> WebDriver:
-    return _driver
+    @classmethod
+    def get_driver(cls) -> webdriver.Chrome:
+        if cls._driver is None:
+            cls._driver = webdriver.Chrome()
+        return cls._driver
 
-
-def set_driver(new_driver: WebDriver) -> None:
-    global _driver
-    _driver = new_driver
+    @classmethod
+    def set_driver(cls, new_driver: webdriver.Chrome) -> None:
+        cls._driver = new_driver
 
 
 def parse_hdd_block_prices(
@@ -71,7 +74,7 @@ def parse_hdd_block_prices(
     detailed_url = urljoin(
         BASE_URL, product_soup.select_one(".title")["href"]
     )
-    driver = get_driver()
+    driver = DriverManager.get_driver()
     driver.get(detailed_url)
     try:
         swatches = driver.find_element(By.CLASS_NAME, "swatches")
@@ -92,29 +95,32 @@ def parse_hdd_block_prices(
 
 
 def parse_rating(product_soup: BeautifulSoup) -> int:
-    try:
-        return int(product_soup.select_one("p[data-rating]")["data-rating"])
-    except TypeError:
+    rating_element = product_soup.select_one("p[data-rating]")
+    if rating_element is not None:
+        return int(rating_element.get("data-rating"))
+    else:
         rating_element = product_soup.select_one(".ratings")
         star_icons = rating_element.select(".ws-icon.ws-icon-star")
-        return len(star_icons)
+    return len(star_icons)
 
 
 def parse_single_product(product_soup: BeautifulSoup) -> Product:
-    # hdd_prices = parse_hdd_block_prices(
-    # product_soup
-    # ) :TODO test does not cover it
+    hdd_prices = parse_hdd_block_prices(
+        product_soup
+    )  # :TODO test does not cover it
     return Product(
         title=product_soup.select_one(".title")["title"],
-        description=product_soup.select_one(".description").text.replace("\xa0", " "),
+        description=product_soup.select_one(
+            ".description"
+        ).text.replace("\xa0", " "),
         price=float(product_soup.select_one(".price").text.replace("$", "")),
         rating=parse_rating(product_soup),
         num_of_reviews=int(
             product_soup.select_one(".ratings > p.pull-right").text.split()[0]
         ),
-        # additional_info={
-        # "hdd_prices": hdd_prices
-        # }, :TODO test does not cover it
+        additional_info={
+            "hdd_prices": hdd_prices
+        },  # :TODO test does not cover it
     )
 
 
@@ -125,31 +131,27 @@ def get_single_page_products(page_soup: BeautifulSoup) -> [Product]:
 
 
 def check_button_more(url: str) -> str:
-    driver = get_driver()
+    driver = DriverManager.get_driver()
     driver.get(url)
-    try:
-        more_button = driver.find_element(
-            By.CLASS_NAME, "ecomerce-items-scroll-more"
-        )
 
-        more = None
-        while more is None:
-            try:
-                driver.find_element(
-                    By.CSS_SELECTOR,
-                    'a.ecomerce-items-scroll-more[style="display: none;"]'
-                )
-                more = 1
-            except NoSuchElementException:
-                driver.execute_script("arguments[0].click();", more_button)
-    except NoSuchElementException:
-        pass
+    cookie_banner = driver.find_element(By.ID, "cookieBanner")
+    if cookie_banner.is_displayed():
+        cookie_banner.find_element(By.ID, "closeCookieBanner").click()
+
+    more_button = driver.find_elements(
+        By.CLASS_NAME, "ecomerce-items-scroll-more"
+    )
+
+    if more_button:
+        while more_button[0].is_displayed():
+            more_button[0].click()
+            time.sleep(0.2)
 
     dynamic_page_source = driver.page_source
     return dynamic_page_source
 
 
-def get_products(url: str) -> [Product]:
+def get_products(url: str) -> list[Product]:
     page = check_button_more(url)
 
     page_soup = BeautifulSoup(page, "html.parser")
@@ -158,7 +160,7 @@ def get_products(url: str) -> [Product]:
     return all_products
 
 
-def write_products_to_csv(products: [Product], path: str) -> None:
+def write_products_to_csv(products: list[Product], path: str) -> None:
     with open(path, "w") as file:
         writer = csv.writer(file)
         writer.writerow(PRODUCT_FIELDS)
@@ -169,7 +171,7 @@ def get_all_products() -> None:
     for url_key in ALL_URLS:
         print(url_key)
         with webdriver.Chrome() as new_driver:
-            set_driver(new_driver)
+            DriverManager.set_driver(new_driver)
             products = get_products(ALL_URLS[url_key])
             write_products_to_csv(products, ALL_PATH[url_key])
 
