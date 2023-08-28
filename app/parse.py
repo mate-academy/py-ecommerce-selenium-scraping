4a5dd136569060b63d63ce0f0d79c5c21d323a36
Anton Shpakovych <shpakovych18@gmail.com>
2023-08-28 20:01:25 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,22 +1,9 @@
-from dataclasses import dataclass
-from urllib.parse import urljoin
-
-
-BASE_URL = "https://webscraper.io/"
-HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
-
-
-@dataclass
-class Product:
-    title: str
-    description: str
-    price: float
-    rating: int
-    num_of_reviews: int
+from app.parser import Parser
 
 
 def get_all_products() -> None:
-    pass
+    parser = Parser()
+    parser.parse()
 
 
 if __name__ == "__main__":
