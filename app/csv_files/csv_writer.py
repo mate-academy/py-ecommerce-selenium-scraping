0390236ce4a6d6b593842756c86b074a5a170281
eduardhabryd <eduardhabryd@gmail.com>
2023-11-09 23:30:38 +0200

    remove path to csv_files folder logic

--- app/csv_files/csv_writer.py
+++ app/csv_files/csv_writer.py
@@ -1,14 +1,10 @@
 import csv
-import os
+
 from app.product import Product
 
 
 def write_products_to_file(products: list[Product], file_name: str) -> None:
-    folder_path = os.path.dirname(__file__)
-    os.makedirs(folder_path, exist_ok=True)
-    full_file_path = os.path.join(folder_path, file_name)
-
-    with open(full_file_path, "w", encoding="utf-8", newline="") as file:
+    with open(file_name, "w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["title", "description", "price", "rating", "num_of_reviews"])
         for product in products:
