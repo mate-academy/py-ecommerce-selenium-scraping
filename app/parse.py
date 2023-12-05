5b2a32f0e61908a1fca9ab507f0f4d06b6e28734
Alex_Freud <NikiforenkoAS@i.ua>
2023-12-05 16:09:12 +0200

    Create class DeviceParser and move all functions as class methods

--- app/parse.py
+++ app/parse.py
@@ -35,97 +35,94 @@ class Product:
 PRODUCT_FIELDS = [field.name for field in fields(Product)]
 
 
-def get_headless_driver() -> WebDriver:
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
+class DeviceParser:
+    def __init__(self):
+        self.driver = self.get_headless_driver()
 
-    return driver
+    def get_headless_driver(self) -> WebDriver:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
 
+        return driver
 
-def cookies_button(driver: WebDriver) -> None:
-    while True:
+    def cookies_button(self) -> None:
         try:
-            accept_cookies = driver.find_element(
+            accept_cookies = self.driver.find_element(
                 By.CLASS_NAME, "acceptCookies"
             )
             accept_cookies.click()
         except Exception:
-            break
+            pass
 
-
-def more_button_scroll(driver: WebDriver) -> None:
-    while True:
-        try:
-            more_button = WebDriverWait(driver, 5).until(
-                expected_conditions.element_to_be_clickable(
-                    (By.CSS_SELECTOR, ".btn.ecomerce-items-scroll-more")
+    def more_button_scroll(self) -> None:
+        while True:
+            try:
+                more_button = WebDriverWait(self.driver, 5).until(
+                    expected_conditions.element_to_be_clickable(
+                        (By.CSS_SELECTOR, ".btn.ecomerce-items-scroll-more")
+                    )
                 )
-            )
-            more_button.click()
-        except Exception:
-            break
+                more_button.click()
+            except Exception:
+                break
 
+    def parse_single_product(self, product_soup: Tag) -> Product:
+        return Product(
+            title=product_soup.select_one(".title")["title"],
+            description=(product_soup.select_one(".description")
+                         .text.replace("\xa0", " ")),
+            price=float(product_soup.select_one(".price")
+                        .text.replace("$", "")),
+            rating=len(product_soup.select(".ws-icon")),
+            num_of_reviews=int(product_soup.select_one(".review-count")
+                               .text.split()[0]),
+        )
 
-def parse_single_product(product_soup: Tag) -> Product:
-    return Product(
-        title=product_soup.select_one(".title")["title"],
-        description=(product_soup.select_one(".description")
-                     .text.replace("\xa0", " ")),
-        price=float(product_soup.select_one(".price")
-                    .text.replace("$", "")),
-        rating=len(product_soup.select(".ws-icon")),
-        num_of_reviews=int(product_soup.select_one(".review-count")
-                           .text.split()[0]),
-    )
+    def write_products_info(
+            self,
+            products_list: list[Product],
+            output_csv_path: str
+    ) -> None:
+        with open(
+                output_csv_path,
+                "w",
+                encoding="utf-8",
+                newline=""
+        ) as product_file:
+            writer = csv.writer(product_file)
+            writer.writerow(PRODUCT_FIELDS)
+            for product in products_list:
+                writer.writerow(
+                    [
+                        product.title,
+                        product.description,
+                        product.price,
+                        product.rating,
+                        product.num_of_reviews,
+                    ]
+                )
 
+    def get_all_products(self) -> None:
+        for name, link in tqdm(
+                PAGES.items(),
+                desc="Processing download",
+                unit="page"
+        ):
+            page = urljoin(BASE_URL, link)
+            self.driver.get(page)
+            self.cookies_button()
+            self.more_button_scroll()
+            html_page = self.driver.page_source
+            page_soup = BeautifulSoup(html_page, "html.parser")
+            products_soup = page_soup.select(".card-body")
+            products = [
+                self.parse_single_product(product) for product in products_soup
+            ]
+            self.write_products_info(products, f"{name}.csv")
 
-def write_products_info(
-        products_list: list[Product],
-        output_csv_path: str
-) -> None:
-    with open(
-            output_csv_path,
-            "w",
-            encoding="utf-8",
-            newline=""
-    ) as product_file:
-        writer = csv.writer(product_file)
-        writer.writerow(PRODUCT_FIELDS)
-        for product in products_list:
-            writer.writerow(
-                [
-                    product.title,
-                    product.description,
-                    product.price,
-                    product.rating,
-                    product.num_of_reviews,
-                ]
-            )
-
-
-def get_all_products() -> None:
-    driver = get_headless_driver()
-
-    for name, link in tqdm(
-            PAGES.items(),
-            desc="Processing download",
-            unit="page"
-    ):
-        page = urljoin(BASE_URL, link)
-        driver.get(page)
-        cookies_button(driver)
-        more_button_scroll(driver)
-        html_page = driver.page_source
-        page_soup = BeautifulSoup(html_page, "html.parser")
-        products_soup = page_soup.select(".card-body")
-        products = [
-            parse_single_product(product) for product in products_soup
-        ]
-        write_products_info(products, f"{name}.csv")
-
-    driver.quit()
+        self.driver.quit()
 
 
 if __name__ == "__main__":
-    get_all_products()
+    DeviceParser().get_all_products()
