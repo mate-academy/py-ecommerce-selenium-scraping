70cacaf6fe6415b5cfe7b14a152959316568cd08
VasylTurok <turokvasil0@gmail.com>
2024-01-13 20:43:04 +0200

    solution

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,28 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, astuple, fields
 from urllib.parse import urljoin
 
+from bs4 import BeautifulSoup, Tag
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 BASE_URL = "https://webscraper.io/"
-HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
+
+URLS = {
+    "home.csv": urljoin(BASE_URL, "test-sites/e-commerce/more/"),
+    "computers.csv": urljoin(
+        BASE_URL, "test-sites/e-commerce/more/computers/"
+    ),
+    "laptops.csv": urljoin(
+        BASE_URL, "test-sites/e-commerce/more/computers/laptops"
+    ),
+    "tablets.csv": urljoin(
+        BASE_URL, "test-sites/e-commerce/more/computers/tablets"
+    ),
+    "phones.csv": urljoin(BASE_URL, "test-sites/e-commerce/more/phones/"),
+    "touch.csv": urljoin(BASE_URL, "test-sites/e-commerce/more/phones/touch")
+}
 
 
 @dataclass
@@ -15,8 +34,66 @@ class Product:
     num_of_reviews: int
 
 
+PRODUCT_FIELDS = [field.name for field in fields(Product)]
+
+_driver: WebDriver | None = None
+
+
+def get_driver() -> WebDriver:
+    return _driver
+
+
+def set_driver(new_driver: WebDriver) -> None:
+    global _driver
+    _driver = new_driver
+
+
+def parse_single_product(product_soup: Tag) -> Product:
+    return Product(
+        title=product_soup.select_one(".title")["title"],
+        description=product_soup.select_one("p.description").text.replace(
+            "\xa0", " "
+        ),
+        price=float(product_soup.select_one(".price").text[1:]),
+        rating=len(product_soup.select("span.ws-icon-star")),
+        num_of_reviews=int(
+            product_soup.select_one(".review-count").text.split()[0]
+        ),
+    )
+
+
+def parse_page(url: str) -> [Product]:
+    driver = get_driver()
+    driver.get(url)
+    try:
+        more_button = driver.find_element(By.CLASS_NAME, "btn")
+    except Exception:
+        more_button = None
+    if more_button:
+        while not more_button.get_property("style"):
+            driver.execute_script("arguments[0].click();", more_button)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    product_cards = soup.select(".thumbnail")
+    products = [parse_single_product(product) for product in product_cards]
+
+    return products
+
+
+def write_products_to_csv(products: [Product], file_name: str) -> None:
+    with open(file_name, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(PRODUCT_FIELDS)
+        writer.writerows([astuple(product) for product in products])
+
+
 def get_all_products() -> None:
-    pass
+    with webdriver.Chrome() as new_deriver:
+        set_driver(new_deriver)
+        for file_name, url in URLS.items():
+            products = parse_page(url=url)
+            write_products_to_csv(products=products, file_name=file_name)
 
 
 if __name__ == "__main__":
