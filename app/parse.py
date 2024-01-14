ebc35c3a0ccc023e4727062febeb754661e561dd
VasylTurok <turokvasil0@gmail.com>
2024-01-14 15:48:41 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -35,8 +35,8 @@ class Product:
     rating: int
     num_of_reviews: int
 
-    @staticmethod
-    def parse_single_product(product_soup: Tag) -> "Product":
+    @classmethod
+    def parse_single_product(cls, product_soup: Tag) -> "Product":
         return Product(
             title=product_soup.select_one(".title")["title"],
             description=product_soup.select_one("p.description").text.replace(
@@ -55,21 +55,7 @@ PRODUCT_FIELDS = [field.name for field in fields(Product)]
 _driver: WebDriver | None = None
 
 
-def parse_single_product(product_soup: Tag) -> Product:
-    return Product(
-        title=product_soup.select_one(".title")["title"],
-        description=product_soup.select_one("p.description").text.replace(
-            "\xa0", " "
-        ),
-        price=float(product_soup.select_one(".price").text[1:]),
-        rating=len(product_soup.select("span.ws-icon-star")),
-        num_of_reviews=int(
-            product_soup.select_one(".review-count").text.split()[0]
-        ),
-    )
-
-
-def parse_page(url: str) -> [Product]:
+def parse_page(url: str) -> list[Product]:
     driver = _driver
     driver.get(url)
     try:
@@ -88,7 +74,7 @@ def parse_page(url: str) -> [Product]:
     return products
 
 
-def write_products_to_csv(products: [Product], file_name: str) -> None:
+def write_products_to_csv(products: list[Product], file_name: str) -> None:
     with open(file_name, "w") as file:
         writer = csv.writer(file)
         writer.writerow(PRODUCT_FIELDS)
