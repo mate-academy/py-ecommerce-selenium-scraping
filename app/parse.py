6a22df3d5f3c3b6d63966aaed38a43be2b85fd62
Bohdan Kolomiiets <kb907406@gmail.com>
2024-02-23 19:41:20 +0200

    Add main function for parse

--- app/parse.py
+++ app/parse.py
@@ -1,23 +1,10 @@
-from dataclasses import dataclass
-from urllib.parse import urljoin
+from app.scrapper import Scrapper
 
 
-BASE_URL = "https://webscraper.io/"
-HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
-
-
-@dataclass
-class Product:
-    title: str
-    description: str
-    price: float
-    rating: int
-    num_of_reviews: int
-
-
-def get_all_products() -> None:
-    pass
+def main() -> None:
+    scrapper = Scrapper()
+    scrapper.get_all_products()
 
 
 if __name__ == "__main__":
-    get_all_products()
+    main()
